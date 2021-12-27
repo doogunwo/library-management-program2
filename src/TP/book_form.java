9ded24cd5ce3b14f17fb4a6300460c9989e43a77
도건우 <87505243+doogunwo@users.noreply.github.com>
2021-12-27 13:33:46 +0900

    Add files via upload

--- src/TP/book_form.java
+++ src/TP/book_form.java
@@ -39,7 +39,6 @@ import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.SystemColor;
 import java.awt.Button;
-import javax.swing.JTextArea;
 
 public class book_form extends JFrame {
 
@@ -49,9 +48,6 @@ public class book_form extends JFrame {
 	private JTextField Autior_TF;
 	private JTextField Price_TF;
 	private JTextField Pub_TF;
-	private JTextArea textArea;
-	String filePath;
-	private JTextField textField;
 	dbConnector dbConn = new dbConnector();
 
 	/**
@@ -73,6 +69,8 @@ public class book_form extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	String filePath;
+
 	public book_form() {
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -103,35 +101,33 @@ public class book_form extends JFrame {
 		JLabel lblNewLabel_3 = new JLabel("\uAC00\uACA9");
 		lblNewLabel_3.setBounds(12, 112, 57, 15);
 		panel.add(lblNewLabel_3);
-
-		textArea = new JTextArea(15, 30);
-		textArea.setBounds(81, 171, 255, 85);
-		panel.add(textArea);
-
-		JButton IN_Button = new JButton("\uB4F1\uB85D"); // 등록
+		
+		// 신규 도서 등록버튼 
+		JButton IN_Button = new JButton("\uB4F1\uB85D");
 		IN_Button.setBackground(SystemColor.activeCaption);
-		IN_Button.setBounds(12, 358, 97, 23);
-		panel.add(IN_Button);
 		IN_Button.addActionListener(new ActionListener() {
-
 			public void actionPerformed(ActionEvent e) {
 
 				JLabel aaa = new JLabel("");
 				String sql = "insert into BOOK_TABLE(BOOK_ISBN, BOOK_TITLE, BOOK_AUTHIR, BOOK_PUB,BOOK_PRICE,BOOK_IMAGE,BOOK_PRE) values(?,?,?,?,?,?,?)";
 				Connection tmpConn = dbConn.getConnection();
-
-
 				try {
+
 					PreparedStatement ps = tmpConn.prepareStatement(sql);
 					ps.setString(1, ISBN_TF.getText());
 					ps.setString(2, title_TF.getText());
 					ps.setString(3, Autior_TF.getText());
 					ps.setString(4, Pub_TF.getText());
 					ps.setString(5, Price_TF.getText());
+
 					File tmpFile = new File(filePath);
+
 					ps.setBinaryStream(6, new FileInputStream(tmpFile), tmpFile.length());
+
 					ps.setString(7, "1");
+
 					int count = ps.executeUpdate();
+
 					if (count == 0) {
 						JOptionPane.showMessageDialog(null, "도서 : " + title_TF.getText() + "이(는) 등록에 실패하였습니다.",
 								"신규도서등록", JOptionPane.ERROR_MESSAGE);
@@ -139,40 +135,25 @@ public class book_form extends JFrame {
 						JOptionPane.showMessageDialog(null, "도서 : " + title_TF.getText() + "이(는) 등록이 완료되었습니다.",
 								"신규도서등록", JOptionPane.NO_OPTION);
 					}
-				}
-				catch (SQLException e1) {
+
+				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				} catch (FileNotFoundException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				String sql2 = "insert into BOOK_TABLE_ADDITIONAL_INFORMATION(BOOK_ISBN, BOOK_DESCPIPTION, BOOK_LINK)values(?,?,?)";
-				Connection tmpConn2 = dbConn.getConnection();
-				try {	//줄거리와 링크를 입력하는 try 문
-					PreparedStatement ps2 = tmpConn2.prepareStatement(sql2);
-					ps2.setString(1, ISBN_TF.getText());
-					ps2.setString(2, textArea.getText());
-					ps2.setString(3, textField.getText());
-					
-				}catch (SQLException e2) {
-					// TODO Auto-generated catch block
-					e2.printStackTrace();
-				}
-				
 				ISBN_TF.setText("");
 				title_TF.setText("");
 				Autior_TF.setText("");
 				Pub_TF.setText("");
 				Price_TF.setText("");
-				
-				textField.setText("");
-				textArea.setText("");
 			}
 
 		});
 
-
+		IN_Button.setBounds(12, 174, 97, 23);
+		panel.add(IN_Button);
 
 		ISBN_TF = new JTextField();
 		ISBN_TF.setBounds(81, 7, 116, 21);
@@ -198,6 +179,7 @@ public class book_form extends JFrame {
 		panel.add(Price_TF);
 		Price_TF.setColumns(10);
 
+
 		JFileChooser memberImg = new JFileChooser();
 		JButton Upload_Button = new JButton("\uC0AC\uC9C4 \uB4F1\uB85D");
 		Upload_Button.setBackground(SystemColor.activeCaption);
@@ -216,15 +198,18 @@ public class book_form extends JFrame {
 		});
 		Upload_Button.setBounds(358, 207, 97, 23);
 		panel.add(Upload_Button);
-
-		JButton IN_Button_1 = new JButton("\uB3C4\uC11C \uC0AD\uC81C"); // 도서 삭제
+		
+		//ISBN이 PK 값이라 지울수 없어서 BOOK_PRE 값을 2로 변경하여 BOOK_PRE 값 2인 도서는 삭제된 도서로 간주 
+		//BOOK_PRE == 1 존재 BOOK_PRE == 0 대출중 BOOK_PRE == 2 삭제된 도서 
+		
+		JButton IN_Button_1 = new JButton("\uB3C4\uC11C \uC0AD\uC81C");
 		IN_Button_1.setBackground(SystemColor.activeCaption);
-		IN_Button_1.setBounds(121, 358, 97, 23);
+		IN_Button_1.setBounds(121, 174, 97, 23);
 		panel.add(IN_Button_1);
 		IN_Button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Connection tmpConn = dbConn.getConnection();
-				String str3 = "UPDATE BOOK_TABLE SET BOOK_PRE = 0 where BOOK_ISBN = '" + ISBN_TF.getText() + "' ;";
+				String str3 = "UPDATE BOOK_TABLE SET BOOK_PRE = 2 where BOOK_ISBN = '" + ISBN_TF.getText() + "' ;";
 				try {
 					PreparedStatement ps = tmpConn.prepareStatement(str3);
 					int src3 = dbConn.executeUpdate(str3);
@@ -236,9 +221,9 @@ public class book_form extends JFrame {
 			}
 		});
 
-		JButton btnNewButton_6 = new JButton("\uD655\uC778"); // 확인 버튼
+		JButton btnNewButton_6 = new JButton("\uD655\uC778");
 		btnNewButton_6.setBackground(SystemColor.activeCaption);
-		btnNewButton_6.setBounds(213, 6, 97, 23);
+		btnNewButton_6.setBounds(256, 83, 97, 23);
 		panel.add(btnNewButton_6);
 
 		btnNewButton_6.addActionListener(new ActionListener() { // panel_4
@@ -246,9 +231,6 @@ public class book_form extends JFrame {
 
 				String str = "select BOOK_TITLE, BOOK_AUTHIR, BOOK_PUB, BOOK_PRICE, BOOK_PRE, BOOK_IMAGE FROM BOOK_TABLE WHERE BOOK_ISBN = "
 						+ "'" + ISBN_TF.getText() + "'" + ";";
-				// 줄거리와 링크를 가져오는 sql 문
-				String srr2 = "select * FROM BOOK_TABLE_ADDITIONAL_INFORMATION WHERE BOOK_ISBN = " + "'"
-						+ ISBN_TF.getText() + "'" + ";";
 				try {
 					ResultSet src = dbConn.executeQurey(str);
 					while (src.next()) {
@@ -259,26 +241,15 @@ public class book_form extends JFrame {
 						Price_TF.setText(src.getString("BOOK_PRICE"));
 					}
 				} catch (Exception e1) {
-
-				}
-				// db에 접속하여 입력한 isbn 값과 같은 행의 줄거리와 링크를 가져옴
-				try {
-					ResultSet src2 = dbConn.executeQurey(srr2);
-					if (src2.next()) {
-						textArea.setText(src2.getString(2));
-						textField.setText(src2.getString(3));
-					}
-				} catch (Exception e1) {
-
 				}
 			}
 		});
 
+		//중복 도서 체크 버튼 
 		JButton btnNewButton_6_1 = new JButton("\uC911\uBCF5 \uD655\uC778");
 		btnNewButton_6_1.setBackground(SystemColor.activeCaption);
 		btnNewButton_6_1.setBounds(197, 31, 97, 23);
 		panel.add(btnNewButton_6_1);
-
 		JLabel aaa = new JLabel("");
 		btnNewButton_6_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -297,7 +268,7 @@ public class book_form extends JFrame {
 					ResultSet rs2 = stmt.executeQuery(string_Oder);
 
 					while (rs2.next()) {
-						if (ISBN_TF.getText().equals(rs2.getString("BOOK_ISBN"))) {
+					if (ISBN_TF.getText().equals(rs2.getString("BOOK_ISBN"))) {
 							JOptionPane.showMessageDialog(null, "이미 등록된 도서 입니다.", "Message", JOptionPane.ERROR_MESSAGE);
 							cnt++;
 							break;
@@ -336,113 +307,8 @@ public class book_form extends JFrame {
 			}
 		});
 		membershipListButton.setBackground(SystemColor.activeCaption);
-		membershipListButton.setBounds(12, 401, 154, 23);
+		membershipListButton.setBounds(12, 217, 154, 23);
 		panel.add(membershipListButton);
-
-		JLabel lblNewLabel_3_2 = new JLabel("\uC904\uAC70\uB9AC");
-		lblNewLabel_3_2.setBounds(12, 176, 57, 15);
-		panel.add(lblNewLabel_3_2);
-
-		JLabel lblNewLabel_3_2_1 = new JLabel("\uB9C1\uD06C");
-		lblNewLabel_3_2_1.setBounds(12, 137, 57, 15);
-		panel.add(lblNewLabel_3_2_1);
-
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(81, 134, 213, 21);
-		panel.add(textField);
-
-		JButton IN_Button_1_1 = new JButton("\uB3C4\uC11C \uC218\uC815"); // 도서 수정
-		IN_Button_1_1.setBackground(SystemColor.activeCaption);
-		IN_Button_1_1.setBounds(230, 358, 97, 23);
-		panel.add(IN_Button_1_1);
-		IN_Button_1_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				if (ISBN_TF.getText().equals("")) {
-					JOptionPane.showMessageDialog(null, "도서의 ISBN을 입력해 주세요", "Message", JOptionPane.ERROR_MESSAGE);
-				} else {
-					// 먼저 책이 존재 하는지 판단
-					String sql = "select BOOK_PRE FROM BOOK_TABLE WHERE BOOK_ISBN = " + "'" + ISBN_TF.getText() + "'"
-							+ ";";
-					ResultSet Src = dbConn.executeQurey(sql);
-					try {
-						if (Src.next()) {
-							if (Src.getInt(1) == 1) {
-								if (!title_TF.getText().equals("")) {
-									String str = "update BOOK_TABLE SET BOOK_TITLE = '" + title_TF.getText()
-											+ "' WHERE BOOK_ISBN = " + "'" + ISBN_TF.getText() + "'" + ";";
-									// System.out.println(str);
-									int src = dbConn.executeUpdate(str);
-									src = 0;
-									str = null;
-								}
-								if (!Autior_TF.getText().equals("")) {
-									String str = "update BOOK_TABLE SET BOOK_AUTHIR = '" + Autior_TF.getText()
-											+ "' WHERE BOOK_ISBN = " + "'" + ISBN_TF.getText() + "'" + ";";
-									// System.out.println(str);
-									int src = dbConn.executeUpdate(str);
-									src = 0;
-									str = null;
-
-								}
-								if (!Price_TF.getText().equals("")) {
-									String str = "update BOOK_TABLE SET BOOK_PRICE = '" + Price_TF.getText()
-											+ "' WHERE BOOK_ISBN = " + "'" + ISBN_TF.getText() + "'" + ";";
-									// System.out.println(str);
-									int src = dbConn.executeUpdate(str);
-									src = 0;
-									str = null;
-
-								}
-								if (!Pub_TF.getText().equals("")) {
-									String str = "update BOOK_TABLE SET BOOK_PUB = '" + Pub_TF.getText()
-											+ "' WHERE BOOK_ISBN = " + "'" + ISBN_TF.getText() + "'" + ";";
-									// System.out.println(str);
-									int src = dbConn.executeUpdate(str);
-									src = 0;
-									str = null;
-								}
-								if (!textField.getText().equals("")) {
-									String str = "update BOOK_TABLE_ADDITIONAL_INFORMATION SET BOOK_LINK = '"
-											+ textField.getText() + "' WHERE BOOK_ISBN = " + "'" + ISBN_TF.getText()
-											+ "'" + ";";
-									// System.out.println(str);
-									int src = dbConn.executeUpdate(str);
-									src = 0;
-									str = null;
-								}
-								if (!textArea.getText().equals("")) {
-									String str = "update BOOK_TABLE_ADDITIONAL_INFORMATION SET BOOK_DESCPIPTION= '"
-											+ textArea.getText() + "' WHERE BOOK_ISBN = " + "'" + ISBN_TF.getText()
-											+ "'" + ";";
-									// System.out.println(str);
-									int src = dbConn.executeUpdate(str);
-									src = 0;
-									str = null;
-								}
-
-							} else {
-								JOptionPane.showMessageDialog(null, "도서가 존재하지 않습니다.", "Message",
-										JOptionPane.ERROR_MESSAGE);
-							}
-						}
-					} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-
-					// 수정후 메세지 다이얼로그가 나오고 텍트트 필드와 에리어를 전부 지움.
-					JOptionPane.showMessageDialog(null, "도서정보 수정이 완료되었습니다.", "Message", JOptionPane.ERROR_MESSAGE);
-					ISBN_TF.setText("");
-					title_TF.setText("");
-					Autior_TF.setText("");
-					Price_TF.setText("");
-					Pub_TF.setText("");
-					textField.setText("");
-					textArea.setText("");
-				}
-			}
-		});
 
 		JPanel panel_1 = new JPanel();
 		panel_1.setBackground(SystemColor.activeCaption);
